c3891e4338c249ed1bc5e77e44ed171d75f9534c
Anton Agaronov <144243055+AntonAgaronov@users.noreply.github.com>
2025-04-20 22:30:37 +0400

    add host

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5000"},
+		AllowOrigins:     []string{"http://94.131.107.205:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
